3f4616374687fa80ed27b5b7f510fb5482a0be94
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-06-20 12:00:44 +0200

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,16 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const titleElement = document.getElementById('title');
+
+console.dir(titleElement);
+
+console.dir(titleElement.parentElement);
+console.dir(titleElement.nextElementSibling);
+
+const startPointElement = document.getElementById('start-point');
+
+console.dir(startPointElement);
+console.log(
+  `Soy un ${startPointElement.tagName} con el id es ${startPointElement.id} y la clase ${startPointElement.classList} mi padre es un header ${.parentElement} con la clase`
+);
